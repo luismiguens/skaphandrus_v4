a9229c3665a1913abd5ae5202df6f3cf61fdf487
António Couto <antonio.pedro.couto@morfose.net>
2015-05-13 19:18:02 +0000

    Development on user login and registration pages

---
+++ src/Skaphandrus/AppBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Skaphandrus\AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // add your custom field
+        $builder
+            ->add('email', 'email', array('label' => FALSE, 'translation_domain' => 'FOSUserBundle', 'attr' => array('class' => 'form-control', 'placeholder' => 'form.email')))
+            ->add('username', null, array('label' => FALSE, 'translation_domain' => 'FOSUserBundle', 'attr' => array('class' => 'form-control', 'placeholder' => 'form.username')))
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => FALSE, 'attr' => array('placeholder' => 'form.password', 'class' => 'form-control')),
+                'second_options' => array('label' => FALSE, 'attr' => array('placeholder' => 'form.password_confirmation', 'class' => 'form-control')),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ));
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'skaphandrus_user_registration';
+    }
+}
