d561efd333b66debbc76cacb615eb9bcf4fb07f1
António Couto <antonio.pedro.couto@morfose.net>
2015-06-16 18:28:31 +0000

    Development on identification pages

--- src/Skaphandrus/AppBundle/Controller/SkIdentificationSpeciesController.php
+++ src/Skaphandrus/AppBundle/Controller/SkIdentificationSpeciesController.php
@@ -19,45 +19,72 @@ class SkIdentificationSpeciesController extends Controller
      * Lists all SkSpecies entities.
      *
      */
-    public function indexAction($module_id)
-    {
-
+    public function indexAction(Request $request) {        
         $em = $this->getDoctrine()->getManager();
+        
+        // Handle module_id for filter
+        $module_id = ($request_form = $request->request->get('form')) ?
+            $request_form['module_filter_form_id'] : 6;
 
-        $module = $em->getRepository('SkaphandrusAppBundle:SkIdentificationModule')->findOneById($module_id);
+        // Get current module
+        $module = $em->getRepository('SkaphandrusAppBundle:SkIdentificationModule')
+            ->findOneById($module_id);
+
+        // Get all modules for filter
+        $modules = $em->getRepository('SkaphandrusAppBundle:SkIdentificationModule')
+            ->findAll();
+
+        // Create Module filter form
+        $modules_choices = array();
+        foreach ($modules as $m) {
+            $modules_choices[$m->getId()] = $m->translate()->getName();
+        }
+        $modules_form = $this->createFormBuilder(NULL, array('attr' => array('name' => 'module_filter_form', 'id' => 'module_filter_form')))
+            ->setMethod('POST')
+            ->add('module_filter_form_id', 'choice', array(
+                'choices' => $modules_choices,
+                'required' => FALSE,
+                'label' => 'Module',
+                'data' => $module_id,
+            ))
+            ->add('Filter', 'submit')
+            ->getForm();
 
         $entities = array();
         foreach ($module->getGroups() as $group) {
-            $entities = array_merge($entities, $group->getTaxonValue()->getSpecies()->toArray());
+            if ($group->getIsParentModule()) {
+                $entities = array_merge($entities, $group->getTaxonValue()->getSpecies()->toArray());
+            }
         }
 
         return $this->render('SkaphandrusAppBundle:SkIdentificationSpecies:index.html.twig', array(
             'entities' => $entities,
+            'modules_form' => $modules_form->createView(),
         ));
     }
     /**
      * Creates a new SkSpecies entity.
      *
      */
-    public function createAction(Request $request)
-    {
-        $entity = new SkSpecies();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
+    // public function createAction(Request $request)
+    // {
+    //     $entity = new SkSpecies();
+    //     $form = $this->createCreateForm($entity);
+    //     $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
+    //     if ($form->isValid()) {
+    //         $em = $this->getDoctrine()->getManager();
+    //         $em->persist($entity);
+    //         $em->flush();
 
-            return $this->redirect($this->generateUrl('identification_species_admin_edit', array('id' => $entity->getId())));
-        }
+    //         return $this->redirect($this->generateUrl('identification_species_admin_edit', array('id' => $entity->getId())));
+    //     }
 
-        return $this->render('SkaphandrusAppBundle:SkIdentificationSpecies:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
+    //     return $this->render('SkaphandrusAppBundle:SkIdentificationSpecies:new.html.twig', array(
+    //         'entity' => $entity,
+    //         'form'   => $form->createView(),
+    //     ));
+    // }
 
     /**
      * Creates a form to create a SkSpecies entity.
@@ -66,32 +93,32 @@ class SkIdentificationSpeciesController extends Controller
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createCreateForm(SkSpecies $entity)
-    {
-        $form = $this->createForm(new SkSpeciesType(), $entity, array(
-            'action' => $this->generateUrl('identification_species_admin_create'),
-            'method' => 'POST',
-        ));
+    // private function createCreateForm(SkSpecies $entity)
+    // {
+    //     $form = $this->createForm(new SkIdentificationSpeciesType(), $entity, array(
+    //         'action' => $this->generateUrl('identification_species_admin_create'),
+    //         'method' => 'POST',
+    //     ));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+    //     // $form->add('submit', 'submit', array('label' => 'Create'));
 
-        return $form;
-    }
+    //     return $form;
+    // }
 
     /**
      * Displays a form to create a new SkSpecies entity.
      *
      */
-    public function newAction()
-    {
-        $entity = new SkSpecies();
-        $form   = $this->createCreateForm($entity);
+    // public function newAction()
+    // {
+    //     $entity = new SkSpecies();
+    //     $form   = $this->createCreateForm($entity);
 
-        return $this->render('SkaphandrusAppBundle:SkIdentificationSpecies:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
+    //     return $this->render('SkaphandrusAppBundle:SkIdentificationSpecies:new.html.twig', array(
+    //         'entity' => $entity,
+    //         'form'   => $form->createView(),
+    //     ));
+    // }
 
     /**
      * Finds and displays a SkSpecies entity.
@@ -130,12 +157,10 @@ class SkIdentificationSpeciesController extends Controller
         }
 
         $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('SkaphandrusAppBundle:SkIdentificationSpecies:edit.html.twig', array(
             'entity'      => $entity,
             'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -152,9 +177,6 @@ class SkIdentificationSpeciesController extends Controller
             'action' => $this->generateUrl('identification_species_admin_update', array('id' => $entity->getId())),
             'method' => 'PUT',
         ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
         return $form;
     }
     /**
@@ -171,7 +193,13 @@ class SkIdentificationSpeciesController extends Controller
             throw $this->createNotFoundException('Unable to find SkSpecies entity.');
         }
 
-        $deleteForm = $this->createDeleteForm($id);
+        // Set parent ID on embedded forms
+        $embedded = $request->request->get('skaphandrus_appbundle_skspecies');
+        foreach ($embedded['imageRefs'] as &$imageRef) {
+            $imageRef['species'] = $id;
+        }
+        dump($embedded);
+
         $editForm = $this->createEditForm($entity);
         $editForm->handleRequest($request);
 
@@ -184,32 +212,31 @@ class SkIdentificationSpeciesController extends Controller
         return $this->render('SkaphandrusAppBundle:SkIdentificationSpecies:edit.html.twig', array(
             'entity'      => $entity,
             'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
         ));
     }
     /**
      * Deletes a SkSpecies entity.
      *
      */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
+    // public function deleteAction(Request $request, $id)
+    // {
+    //     $form = $this->createDeleteForm($id);
+    //     $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('SkaphandrusAppBundle:SkSpecies')->find($id);
+    //     if ($form->isValid()) {
+    //         $em = $this->getDoctrine()->getManager();
+    //         $entity = $em->getRepository('SkaphandrusAppBundle:SkSpecies')->find($id);
 
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find SkSpecies entity.');
-            }
+    //         if (!$entity) {
+    //             throw $this->createNotFoundException('Unable to find SkSpecies entity.');
+    //         }
 
-            $em->remove($entity);
-            $em->flush();
-        }
+    //         $em->remove($entity);
+    //         $em->flush();
+    //     }
 
-        return $this->redirect($this->generateUrl('identification_species_admin'));
-    }
+    //     return $this->redirect($this->generateUrl('identification_species_admin'));
+    // }
 
     /**
      * Creates a form to delete a SkSpecies entity by id.
@@ -218,13 +245,13 @@ class SkIdentificationSpeciesController extends Controller
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('identification_species_admin_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
-    }
+    // private function createDeleteForm($id)
+    // {
+    //     return $this->createFormBuilder()
+    //         ->setAction($this->generateUrl('identification_species_admin_delete', array('id' => $id)))
+    //         ->setMethod('DELETE')
+    //         ->add('submit', 'submit', array('label' => 'Delete'))
+    //         ->getForm()
+    //     ;
+    // }
 }
