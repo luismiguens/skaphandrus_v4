86376501631b0ddce1d8d9e7dfe2e40dd7d8a010
António Couto <antonio.pedro.couto@morfose.net>
2015-06-23 19:16:14 +0000

    Development on messages pages

---
+++ src/Skaphandrus/AppBundle/Form/FOSMessage/NewThreadMessageFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Skaphandrus\AppBundle\Form\FOSMessage;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Message form type for starting a new conversation
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ */
+class NewThreadMessageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            #->add('recipient', 'fos_user_username')
+            ->add('recipient', 'autocomplete', array(
+                'class' => 'SkaphandrusAppBundle:FosUser',
+                'attr' => array('class' => 'form-control m-b'),
+                'label'=>'form.new_message.label.user', 
+                'required' => false,
+                'attr' => array('class' => 'form-control'),
+            ))
+            ->add('subject', 'text')
+            ->add('body', 'textarea');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'intention'  => 'message',
+        ));
+    }
+
+    // public function getParent()
+    // {
+    //     return 'fos_message_new_thread';
+    // }
+
+    public function getName()
+    {
+        return 'skaphandrus_message_new_thread';
+    }
+}
--- src/Skaphandrus/AppBundle/Resources/public/js/app.js
+++ src/Skaphandrus/AppBundle/Resources/public/js/app.js
@@ -68,6 +68,12 @@ $(document).ready(function () {
     if ($('#skaphandrus_appbundle_skperson_skaphandrusId').length) {
         $('#skaphandrus_appbundle_skperson_skaphandrusId').autocompleter({url_list: '/app_dev.php/en/ajax_search_fosUser/', url_get: '/app_dev.php/en/ajax_get_fosUser/'});
     }
+    
+    
+    // Formulário FOSMessage new message
+    if ($('#message_recipient').length) {
+        $('#message_recipient').autocompleter({url_list: '/app_dev.php/en/ajax_search_fosUser/', url_get: '/app_dev.php/en/ajax_get_fosUser/'});
+    }
 
     
     
