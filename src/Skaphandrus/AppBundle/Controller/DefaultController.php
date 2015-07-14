00d510779491ea01b0239b52f3e880cab67bb53c
Luis Miguens <luis.t.miguens@gmail.com>
2015-07-14 17:10:50 +0000

    associu

--- src/Skaphandrus/AppBundle/Controller/DefaultController.php
+++ src/Skaphandrus/AppBundle/Controller/DefaultController.php
@@ -255,7 +255,7 @@ class DefaultController extends Controller {
 
 //        $photographers = array();
 //        
-        dump($taxon);
+        //dump($taxon);
         
         // $qb = $this->getDoctrine()->getRepository('SkaphandrusAppBundle:FosUser')->getQueryBuilder([$taxon->getTaxonNodeName() => $taxon->getId()], 20);
         $photographers = $this->getDoctrine()->getRepository('SkaphandrusAppBundle:FosUser')
