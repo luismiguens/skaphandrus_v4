3965e147422cf55263574ed94e89ee9810d281e0
Luis Miguens <luis.t.miguens@gmail.com>
2015-07-20 19:13:51 +0000

    updates

--- src/Skaphandrus/AppBundle/Validator/Constraints/ContainsSufficientPointsValidator.php
+++ src/Skaphandrus/AppBundle/Validator/Constraints/ContainsSufficientPointsValidator.php
@@ -27,28 +27,35 @@ class ContainsSufficientPointsValidator extends ConstraintValidator {
 
     public function validate($acquisitions, Constraint $constraint) {
 
-        $sum_selected_points = 0;
-        //somar pontos tendo em consideração apenas questoes definitivas.
-        // 1 - modulo não é gratuito (is_free=false)
-        // 2 - modulo não foi comprado (store=2)
-        // 3 - aquisition is null (quer dizer que é novo = está checado)
 
-        foreach ($acquisitions as $key => $acquisition) {
+        if (count($acquisitions)>0) {
 
-            //dump($acquisition);
+            $sum_selected_points = 0;
+            //somar pontos tendo em consideração apenas questoes definitivas.
+            // 1 - modulo não é gratuito (is_free=false)
+            // 2 - modulo não foi comprado (store=2)
+            // 3 - aquisition is null (quer dizer que é novo = está checado)
 
-            if ($acquisition->getModule()->getIsFree() != TRUE
-                    and $acquisition->getAcquisitionType() != 2
-                    and $acquisition->getId() == null) {
-                $sum_selected_points = $sum_selected_points + $acquisition->getModule()->getPoints();
+
+
+
+            foreach ($acquisitions as $acquisition) {
+
+                //dump($acquisition);
+
+                if ($acquisition->getModule()->getIsFree() != TRUE
+                        and $acquisition->getAcquisitionType() != 2
+                        and $acquisition->getId() == null) {
+                    $sum_selected_points = $sum_selected_points + $acquisition->getModule()->getPoints();
+                }
             }
-        }
-        
-        if ($sum_selected_points > $acquisition->getFosUser()->getSettings()->getPoints()) {
-            $this->context->buildViolation( $constraint->message)
-                    ->setParameter('%modules_points%', $sum_selected_points)
-                    ->setParameter('%user_points%', $acquisition->getFosUser()->getSettings()->getPoints())
-                                    ->addViolation();
+
+            if ($sum_selected_points > $acquisition->getFosUser()->getSettings()->getPoints()) {
+                $this->context->buildViolation($constraint->message)
+                        ->setParameter('%modules_points%', $sum_selected_points)
+                        ->setParameter('%user_points%', $acquisition->getFosUser()->getSettings()->getPoints())
+                        ->addViolation();
+            }
         }
     }
 
