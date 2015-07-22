2f9f239607c9b7c86f3206f806c94d86b5c6d970
Luis Miguens <luis.t.miguens@gmail.com>
2015-07-22 16:45:59 +0000

    updates

--- src/Skaphandrus/AppBundle/Entity/SkPhotoContest.php
+++ src/Skaphandrus/AppBundle/Entity/SkPhotoContest.php
@@ -404,6 +404,34 @@ class SkPhotoContest
     }
     
    
+    
+    
+    
+    public function getAbsolutePath() {
+        return null === $this->image ? null : $this->getUploadRootDir() . '/' . $this->image;
+    }
+
+    public function getWebPath() {
+        return null === $this->image ? null : $this->getUploadDir() . '/' . $this->image;
+    }
+
+    protected function getUploadRootDir() {
+// the absolute directory path where uploaded
+// documents should be saved
+        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
+    }
+
+    protected function getUploadDir() {
+// get rid of the __DIR__ so it doesn't screw up
+// when displaying uploaded doc/image in the view.
+        return 'uploads/contests';
+    }
+    
+    
+    
+    
+    
+    
     /**
      * Get photos
      *
