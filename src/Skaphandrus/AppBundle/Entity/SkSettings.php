f14a138275efc1121775414accaf56e62898ed4e
rubensardinha <rubensardinha@hotmail.com>
2015-10-20 14:54:44 +0000

    seo final

--- src/Skaphandrus/AppBundle/Entity/SkSettings.php
+++ src/Skaphandrus/AppBundle/Entity/SkSettings.php
@@ -74,6 +74,14 @@ class SkSettings
     
     
     /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->updatedAt = new \DateTime();
+    }
+
+    
+    /**
      * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
      * of 'UploadedFile' is injected into this setter to trigger the  update. If this
      * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
