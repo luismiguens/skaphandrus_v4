53994c09b4d84bef41372eb01a71e019706ff9b1
Luis Miguens <luis.t.miguens@gmail.com>
2015-11-25 16:31:39 -0100

    nudi view

--- src/Skaphandrus/AppBundle/Controller/IdentificationController.php
+++ src/Skaphandrus/AppBundle/Controller/IdentificationController.php
@@ -72,6 +72,10 @@ class IdentificationController extends Controller {
          $em = $this->getDoctrine()->getManager();
         $connection = $em->getConnection();
 
+        
+        $character_obj = new \Skaphandrus\AppBundle\Entity\SkIdentificationCharacter();
+        
+        
         $species = $request->query->get('species');
         if (!$species)
             $species = $request->request->get('species');
@@ -137,9 +141,9 @@ class IdentificationController extends Controller {
 
                 //100px 100px
 //                $character['image_url'] = 'http://skaphandrus.com/thumbnails/small/characters/' . $character_obj->getImage();
-                //$character['image_url'] = 'http://skaphandrus.com/media/cache/sk_widen_240/uploads/characters/' . $character_obj->getImage();
+                $character['image_url'] = 'http://skaphandrus.com/media/cache/sk_widen_240/uploads/characters/' . $character_obj->getImage();
 
-                $character['image_url'] = $this->get('liip_imagine.cache.manager')->getBrowserPath($character_obj->getWebPath(), 'sk_widen_240');
+                //$character['image_url'] = $this->get('liip_imagine.cache.manager')->getBrowserPath($character_obj->getWebPath(), 'sk_widen_240');
 
 
                 $character['image_hash'] = $character_obj->getImage();
