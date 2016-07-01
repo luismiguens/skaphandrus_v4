7f4895d975bca85aa439f70c374896f3073aca87
Luis Miguens <luis.t.miguens@gmail.com>
2016-07-01 11:42:57 +0000

    contest in progress

--- src/Skaphandrus/AppBundle/Entity/Repository/SkPhotoContestRepository.php
+++ src/Skaphandrus/AppBundle/Entity/Repository/SkPhotoContestRepository.php
@@ -17,10 +17,12 @@ class SkPhotoContestRepository extends EntityRepository {
         $query = $this->getEntityManager()->createQuery(
                         'SELECT c
             FROM SkaphandrusAppBundle:SkPhotoContest c
-            WHERE c.promoAt >= :today
+            WHERE c.promoAt <= :today
             ORDER BY c.promoAt desc'
                 )->setParameter('today', new \DateTime());
 
+        
+        
         return $query->getResult();
     }
 
